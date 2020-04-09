64c55819e29612724664cf0d216033835fd72ed7
rupid <liujianwei@zuoyebang.com>
2020-04-09 14:22:35 +0800

    feat: 🎸 http 缓存

--- __test__/curry.js
+++ __test__/curry.js
@@ -1,62 +1,72 @@
-// const mutil = (x, y, z) => {
-//   const result = x * y * z;
-//   console.log(result);
-//   return result
+// // const mutil = (x, y, z) => {
+// //   const result = x * y * z;
+// //   console.log(result);
+// //   return result
+// // }
+
+// // const curry1 = function (fn) {
+// //   console.log(fn.length);
+
+// //   const cb = (...args) => {
+// //     if (args.length < fn.length) {
+// //       return function () {
+// //         args = args.concat([].slice.call(arguments))
+// //         return cb.apply(null, args)
+// //       }
+// //     }
+// //     return fn.apply(null, args)
+// //   }
+// //   return cb
+// // }
+
+// // curry1(mutil)(1)(2)(3)
+
+
+// function arg (...args) {
+//   console.log(args);
 // }
 
-// const curry1 = function (fn) {
-//   console.log(fn.length);
+// arg(1, 2, 3, 4)//[1,2,3,4]
+// arg([1, 2, 3])//[ [ 1, 2, 3 ] ]
+// arg({
+//   a: 1,
+//   b: 2
+// })
 
-//   const cb = (...args) => {
-//     if (args.length < fn.length) {
-//       return function () {
-//         args = args.concat([].slice.call(arguments))
-//         return cb.apply(null, args)
+// // [{ a: 1, b: 2 }]
+
+// let obj = { a: 1, b: 2 }
+// // console.log(...obj) //Error
+
+// function add (x, y, z) {
+//   console.log(x + y + z)
+// }
+
+// console.log(add.length);
+
+// function currying (fn) {
+//   let fnLength = fn.length
+//   function cb (...args) {
+//     if (args.length >= fnLength) {
+//       return fn.apply(null, args)
+//     } else {
+//       return function (...arg2) {
+//         let result = args.concat(arg2)
+//         return cb.apply(null, result)
 //       }
 //     }
-//     return fn.apply(null, args)
 //   }
 //   return cb
 // }
 
-// curry1(mutil)(1)(2)(3)
+// currying(add)(1)(2)(3)  //6
 
-
-function arg (...args) {
-  console.log(args);
-}
-
-arg(1, 2, 3, 4)//[1,2,3,4]
-arg([1, 2, 3])//[ [ 1, 2, 3 ] ]
-arg({
-  a: 1,
-  b: 2
-})
-
-// [{ a: 1, b: 2 }]
-
-let obj = { a: 1, b: 2 }
-// console.log(...obj) //Error
-
-function add (x, y, z) {
-  console.log(x + y + z)
-}
-
-console.log(add.length);
-
-function currying (fn) {
-  let fnLength = fn.length
-  function cb (...args) {
-    if (args.length >= fnLength) {
-      return fn.apply(null, args)
-    } else {
-      return function (...arg2) {
-        let result = args.concat(arg2)
-        return cb.apply(null, result)
-      }
-    }
+function calc(){
+  let count=1;
+  return function(){
+    console.log(count++);
   }
-  return cb
 }
 
-currying(add)(1)(2)(3)  //6
+let fn=calc()
+fn()
--- month/202004/README.md
+++ month/202004/README.md
@@ -8,3 +8,14 @@ vue template compiler包含三个处理步骤，按顺序排列如下：
 - parser：模版解释器，功能为从HTML模版转换为AST
 - optimizer：AST优化，处理静态不参与重复渲染的模版片段，对比虚拟dom
 - codegen：代码生成器。基于AST，生成js函数，延迟到运行时运行，生成纯HTML。
+
+### [前端性能优化：细说浏览器渲染的重排与重绘](http://www.imooc.com/article/45936)
+- 浏览器渲染HTML的步骤
+- 什么是重排，重绘，以及之间关系
+
+### [http缓存](https://mp.weixin.qq.com/s/vI2bxaFsFSB5rGC4Bkr8vQ)
+- expires,响应头，绝对时间  200  from cache
+- cache-control 响应头，相对时间 200  from cache
+- Last-Modified(响应头)/If-Modified-Since(请求头)
+- Etag(响应头)/If-None-Math(请求头)
+
