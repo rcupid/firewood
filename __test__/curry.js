897d73fc804c965806e753bb904aac19595a8e64
刘建伟 <liujianwei@liujianweideMacBook-Pro.local>
2020-10-20 09:25:13 +0800

    feat: 🎸 hello,Oct

--- __test__/curry.js
+++ __test__/curry.js
@@ -12,3 +12,9 @@ if (i === len - 1) {
     index = i;
   }
 }
+
+https://h5-sell.zuoyebang.com/static/hy/sell-payment/add-order.html?H5WebPageV=1.5&ZybDisableSlipBack=1&ZybHideTitle=1&__track__=sell-cart-sku-sheet&appId=homework&appid=homework&cacheStrategy=2&channel=appstore&city=%E5%8C%97%E4%BA%AC%E5%B8%82&courseInfo=%5B%7B%22courseId%22%3A2195144%2C%22couponId%22%3A0%7D%2C%7B%22courseId%22%3A2202179%2C%22couponId%22%3A0%7D%5D&cuid=5628ee01372d9b5d3a6715628368a204319b6d0b&dayivc=65&fillHomeIndicator=0&flowPond=%7B%22sellTestH%22%3A%221%22%7D&fr=&hideNativeTitleBar=1&hideNav=1&iOSVersion=13.1.3&isGray=0&isWxYike=&lastfrom=in_course_xuankedan_sell_&logpath=N3&nt=wifi&orifrom=&os=ios&padPhone=1&province=%E5%8C%97%E4%BA%AC%E5%B8%82&saleChannelId=&skuids=%5B2195144%2C2202179%5D&token=2_XPXQH3c5HRPtFHkSwi3sCCURmT25QfxM&unionskuids=%5B%5D&vc=416&vcname=13.1.2&zbkvc=235
+
+http://www.zuoyebang.com/static/hy/yike-sell-app-vue/new-address.html?referPageName=&goback=&isWxYike=&saleChannelId=&lastfrom=&logpath=N38&flowPond=&fr=&orifrom=
+
+http://www.zuoyebang.com/static/hy/yike-sell-app-vue/receive-address-list.html?referPageName=&goback=&isWxYike=&saleChannelId=&lastfrom=&logpath=N38&flowPond=&fr=&orifrom=
---
+++ month/202010/README.md
@@ -0,0 +1,5 @@
+### [brew安装亲测有效](https://www.cnblogs.com/joyce33/p/13376752.html)
+### [Charles破解地址](https://blog.csdn.net/xingchenxuanfeng/article/details/80855948)
+
+### [使用 rollup 打包 JS](https://juejin.im/post/6844903731343933453)
+- rollup-plugin-replace替换环境变量
